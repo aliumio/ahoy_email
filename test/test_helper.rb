1954b0ee562ba41b87922b79f598fe5d4a043683
Donovan Munerlyn <donovan.munerlyn@gmail.com>
2024-03-15 12:49:30 -0400

    Fixed external redirects with Rails 7

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,9 @@ Combustion.initialize! :active_record, :action_mailer do
   if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
-
+  if ActiveRecord::VERSION::MAJOR >= 7
+    config.action_controller.raise_on_open_redirects = true
+  end
   logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
   config.logger = logger
 end
